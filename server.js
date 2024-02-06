69714ce5b83051b0cac2078778fb48cdd6754273
KaoriYes <Quinten.kok@hva.nl>
2024-02-06 15:12:01 +0100

    A lot

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const port = 1337;
 
-const methodOverride = require('method-override');
-const multer = require('multer');
-const upload = multer({
-    dest: 'public/uploads/',
-});
 
 app.use(express.static('public'));
 
