c5092818523c9f8c21ea095594f95a92732633c7
KaoriYes <Quinten.kok@hva.nl>
2024-02-06 15:36:16 +0100

    A lot

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const http = require('http').Server(app);
 const port = 1337;
 
+require("dotenv").config();
+
+const tidalApiToken = process.env.TIDAL_API_TOKEN;
 
 app.use(express.static('public'));
 
@@ -11,6 +14,23 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+const axios = require('axios');
+
+const getTrackInfo = async () => {
+    try {
+        const response = await axios.get('https://api.tidal.com/v1/tracks/251380837?countryCode=US', {
+            headers: {
+                'X-Tidal-Token': tidalApiToken;
+            }
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching track information:', error.response ? error.response.data : error.message);
+    }
+};
+
+getTrackInfo();
 
 http.listen(port, () => {
     console.log('Running on Port: ' + port);
